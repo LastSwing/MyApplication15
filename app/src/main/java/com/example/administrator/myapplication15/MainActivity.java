37c1304c69ef31f25127a41fe5c5d6d34274c2ef
LastSwing <335224756@qq.com>
2016-09-18 12:44:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication15/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.myapplication15;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button tel;
+    private EditText number;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tel = (Button)findViewById(R.id.btn_Tel);
+        number = (EditText)findViewById(R.id.et_number);
+
+        tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + number.getText().toString()));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
